050af5c40ff6aefa17998f47b52954bc29f12f31
KevinBermudezC <kevinbermudezc@gmail.com>
2024-11-03 22:39:28 -0500

    :technologist: ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-white">Welcome to the world of nextjs</h1>
+    <h1 className="text-3xl font-bold text-white">
+      Welcome to the world of nextjs
+    </h1>
   );
 }
